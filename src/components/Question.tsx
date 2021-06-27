335765094a5a4cb2a765039b3183da7c78036573
Marcelo Silveira <silveira.marcelo2010@gmail.com>
2021-06-26 23:38:53 -0300

    Commit - Aula 04 - quase finalizada

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,16 +1,20 @@
-import './styles.scss';
+import { ReactNode } from 'react';
+
+import '../styles/question.scss';
 
 type QuestionProps = {
   content: string;
   author: {
     name: string;
     avatar: string;
-  }
+  };
+  children?: ReactNode;
 }
 
 export function Question({
   content,
   author,
+  children
 }: QuestionProps) {
   return (
     <div className="question">
@@ -19,7 +23,9 @@ export function Question({
         <div className="user-info">
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
-          <div></div>
+        </div>
+        <div>
+          {children}
         </div>
       </footer>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,23 @@
+import { FormEvent, useState } from 'react';
+
 import { useHistory } from 'react-router-dom';
+
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
-import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useAuth()
+  const { user, signInWithGoogle } = useAuth();
+
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
     if (!user) {
@@ -20,33 +26,59 @@ export function Home() {
     history.push('/rooms/new');
   }
 
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('This room doesn\'t exists!');
+      return;
+    }
+
+    if (roomRef.val().endedAt) {
+      alert('Room already closed!');
+      return;
+    }
+
+    history.push(`rooms/${roomCode}`);
+  }
+
   return (
     <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as  dúvidas da sua audiência em tempo-real</p>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
-      <main>
 
+      <main>
         <div className="main-content">
-          <img src={logoImg} alt="Letme ask" />
+          <img src={logoImg} alt="Letmeask" />
+
           <button onClick={handleCreateRoom} className="create-room">
-            <img src={googleIconImg} alt="Logo do Google" />
+            <img src={googleIconImg} alt="Google" />
             Crie sua sala com o Google
           </button>
+
           <div className="separator">ou entre em uma sala</div>
-          <form>
+
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
-            <Button type="submit">
-              Entrar na sala
-            </Button>
+
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
     </div>
-  )
+  );
 }
